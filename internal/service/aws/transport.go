a048affce2d8b59f7d22bcf5b54b7bc67f7d8df5
Mitchell Wilson <mitchelldw01@gmail.com>
2024-04-22 20:07:59 -0600

    feat: implement AWS S3 transport

--- internal/service/aws/transport.go
+++ internal/service/aws/transport.go
@@ -1,10 +1,14 @@
 package aws
 
 import (
-	"bytes"
+	"context"
+	"fmt"
 	"io"
-	"strings"
+	"os"
+	"path"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
@@ -22,18 +26,63 @@ func NewAwsTransport(client *s3.Client, project, bucket string) AwsTransport {
 	}
 }
 
-func (at AwsTransport) Reader(key string) (io.ReadCloser, error) {
-	return io.NopCloser(strings.NewReader("stubbed response")), nil
+func (t AwsTransport) Reader(key string) (io.ReadCloser, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	res, err := t.client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(t.bucket),
+		Key:    aws.String(path.Join(t.project, key)),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to read cache asset: %v", err)
+	}
+
+	return res.Body, nil
 }
 
-func (at AwsTransport) Writer(key string) (io.WriteCloser, error) {
-	return &stubWriteCloser{Buffer: new(bytes.Buffer)}, nil
+func (t AwsTransport) Writer(key string) (io.WriteCloser, error) {
+	tmp, err := os.CreateTemp("", "aws-")
+	if err != nil {
+		return nil, fmt.Errorf("failed to write cache asset: %v", err)
+	}
+
+	return &AwsUploader{
+		client: t.client,
+		file:   tmp,
+		bucket: t.bucket,
+		key:    path.Join(t.project, key),
+	}, nil
 }
 
-type stubWriteCloser struct {
-	*bytes.Buffer
+type AwsUploader struct {
+	client *s3.Client
+	file   *os.File
+	bucket string
+	key    string
 }
 
-func (swc *stubWriteCloser) Close() error {
+func (u *AwsUploader) Write(b []byte) (int, error) {
+	return u.file.Write(b)
+}
+
+func (u *AwsUploader) Close() error {
+	defer u.file.Close()
+	if _, err := u.file.Seek(0, io.SeekStart); err != nil {
+		return fmt.Errorf("failed to write cache asset: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	_, err := u.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(u.bucket),
+		Key:    aws.String(u.key),
+		Body:   u.file,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to write cache asset: %v", err)
+	}
+
 	return nil
 }
---
+++ internal/service/aws/transport_test.go
@@ -0,0 +1,74 @@
+package aws_test
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	omniAws "github.com/mitchelldw01/omnirepo/internal/service/aws"
+)
+
+const (
+	key  = "test.txt"
+	body = "lorem ipsum dolor sit amet"
+)
+
+func TestReader(t *testing.T) {
+	tester, err := newAwsTester()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := tester.createTestObject(); err != nil {
+		t.Fatal(err)
+	}
+
+	trans := omniAws.NewAwsTransport(tester.client, project, bucket)
+	r, err := trans.Reader(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+
+	buf := new(strings.Builder)
+	if _, err = io.Copy(buf, r); err != nil {
+		t.Fatalf("failed to read from reader: %v", err)
+	}
+
+	if res := buf.String(); res != body {
+		t.Fatalf("expected %q, got %q", body, res)
+	}
+}
+
+func TestWriter(t *testing.T) {
+	tester, err := newAwsTester()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := tester.deleteTestObject(); err != nil {
+		t.Fatal(err)
+	}
+
+	trans := omniAws.NewAwsTransport(tester.client, project, bucket)
+	w, err := trans.Writer(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = w.Write([]byte(body))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	buf, err := tester.readTestObject()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res := buf.String(); res != body {
+		t.Errorf("expected %v, got %v", body, res)
+	}
+}
