66591dfa9fd90d6ff71364c9a3d9d5f2ff461b7e
Mitchell Wilson <mitchelldw01@gmail.com>
2024-04-23 20:11:31 -0600

    feat: implement system cache lock

--- internal/service/sys/lock.go
+++ internal/service/sys/lock.go
@@ -1,16 +1,39 @@
 package sys
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 type SystemLock struct {
 	path string
 }
 
 func NewSystemLock() (SystemLock, error) {
-	return SystemLock{path: ""}, nil
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return SystemLock{}, fmt.Errorf("failed to initialize cache lock: %v", err)
+	}
+
+	path := filepath.Join(home, ".omni/cache/lock")
+	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return SystemLock{}, err
+	}
+
+	return SystemLock{path: path}, nil
 }
 
 func (l SystemLock) Lock() error {
+	file, err := os.OpenFile(l.path, os.O_CREATE|os.O_EXCL, 0o644)
+	if err != nil {
+		if os.IsExist(err) {
+			return fmt.Errorf("lock is already acquired... run 'omni unlock' to cancel")
+		}
+		return err
+	}
+
+	file.Close()
 	return nil
 }
 
